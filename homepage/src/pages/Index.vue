61ba0d7d7ec5aa3b72cdedd03a59814732a3d259
GameChaner <941994464@qq.com>
2019-12-18 10:11:49 +0800

    first commit

---
+++ homepage/src/pages/Index.vue
@@ -0,0 +1,89 @@
+<template>
+  <div>
+    <div class="page-header clear-filter" filter-color="orange">
+      <parallax
+        class="page-header-image"
+        style="background-image:url('img/bg11.jpg')"
+      >
+      </parallax>
+      <div class="container">
+
+        <div class="content-center brand">
+          <img class="n-logo" src="img/now-logo.png" alt="" />
+          
+          <h1 class="h1-seo">V N F</h1>
+          <h3>Exploring Visual Narrative Flows in Infographics</h3>
+          
+        </div>
+        <h6 class="category category-absolute">Designed by vizgroup &nbsp;&nbsp;
+          </h6>
+      </div>
+    </div>
+    <contribution></contribution>
+    <!-- <basic-elements></basic-elements>
+    <navigation></navigation>
+    <tabs-section></tabs-section>
+    <progress-pagination></progress-pagination>
+    <notifications></notifications>
+    <typography></typography>
+    <javascript-components></javascript-components>
+    <carousel-section></carousel-section>
+    <nucleo-icons-section></nucleo-icons-section> -->
+    <!-- <div class="section">
+      <div class="container text-center">
+        <div class="row justify-content-md-center">
+          <div class="col-md-12 col-lg-8">
+            <h2 class="title">Completed with examples</h2>
+            <h5 class="description">
+              The kit comes with three pre-built pages to help you get started
+              faster. You can change the text and images and you're good to go.
+              More importantly, looking at them will give you a picture of what
+              you can built with this powerful Bootstrap 4 ui kit.
+            </h5>
+          </div>
+        </div>
+      </div>
+    </div> -->
+    <!-- <signup-form></signup-form> -->
+    <examples-section></examples-section>
+    <!-- <download-section></download-section> -->
+  </div>
+</template>
+<script>
+import { Parallax } from '@/components';
+import BasicElements from './components/BasicElementsSection';
+import Navigation from './components/Navigation';
+import TabsSection from './components/Tabs';
+import ProgressPagination from './components/ProgressPagination';
+import Notifications from './components/Notifications';
+import Typography from './components/Typography';
+import JavascriptComponents from './components/JavascriptComponents';
+import CarouselSection from './components/CarouselSection';
+import NucleoIconsSection from './components/NucleoIconsSection';
+import SignupForm from './components/SignupForm';
+import ExamplesSection from './components/ExamplesSection';
+import DownloadSection from './components/DownloadSection';
+import Contribution from "./Contribution";
+
+export default {
+  name: 'index',
+  bodyClass: 'index-page',
+  components: {
+    Parallax,
+    BasicElements,
+    Navigation,
+    TabsSection,
+    ProgressPagination,
+    Notifications,
+    Typography,
+    JavascriptComponents,
+    CarouselSection,
+    NucleoIconsSection,
+    SignupForm,
+    ExamplesSection,
+    DownloadSection,
+    Contribution,
+  }
+};
+</script>
+<style></style>
---
+++ homepage/src/pages/components/ExamplesSection.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="section section-examples" data-background-color="black">
+    <div class="space-50"></div>
+    <div class="container text-center">
+      <div class="row">
+        <div class="col">
+          <a href="http://47.103.22.185:8088/" target="_blank">
+            <img src="img/VNF-EXPLORER.png" alt="Image" class="img-raised" />
+          </a>
+          <a
+            href="http://47.103.22.185:8088/"
+            class="btn btn-simple btn-primary btn-round"
+            >View VNF-EXPLORER
+        </a>
+        </div>
+        <div class="col">
+          <a href="http://47.103.22.185:8089/" target="_blank">
+            <img src="img/output.png" alt="Image" class="img-raised" />
+          </a>
+          <a
+            href="http://47.103.22.185:8089/"
+            class="btn btn-simple btn-primary btn-round"
+            >View Dataset</a
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style></style>
